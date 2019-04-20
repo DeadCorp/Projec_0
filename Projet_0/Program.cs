5f6a8faef642ccb937d8b6655d185c866798dcb5
DeadCorp <44110067+DeadCorp@users.noreply.github.com>
2019-04-20 22:33:05 +0300

    Original

---
+++ Projet_0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Projet_0
+{
+    
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projet_0/Rew.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_0
+{
+    class Rew
+    {
+        public int s1;
+        public int s2;
+        public int s3;
+        
+
+    }
+}

598ec5737eb02c72e81c240851f9c770e887ce56
DeadCorp <44110067+DeadCorp@users.noreply.github.com>
2019-04-21 21:53:44 +0300

    - Add folder  Example_Class - Add new example class - Some class heve benn moved to new folder

--- Projet_0/Program.cs
+++ Projet_0/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 
 namespace Projet_0
 {
     
-
-    internal class Program
+    internal  class Program
     {
-        private static void Main()
-        {
-            
+        private static void Main(){
+            var tuple = (years_passed: 10, term: 25);
             Console.WriteLine("Allo");
             Console.WriteLine("It's add in new branch");
+            int[] h = { 10, 2, 3, 4, 5 };
+            foreach (var num in h)
+            {
+                Console.WriteLine("Value" + num);
+            }
+
+
 
             Console.ReadKey();
         }
